f849e6668c36781cda496fcbdfdddea4d11bff7b
Brewlyn Surge <brewlynsurge@gmail.com>
2026-05-10 23:20:48 +0530

    Reinitializing the entire project

--- spider/src/main.rs
+++ spider/src/main.rs
@@ -1,16 +1,3 @@
-pub mod crawler;
-pub mod indexer;
-pub mod spider;
-pub mod url_frontier;
-pub mod utils;
-
-#[tokio::main]
-async fn main() {
-    let spider_info = utils::Info::new("Spider");
-    spider_info.display_head();
-
-    let mut spider_crawler = spider::Spider::new("data");
-    spider_crawler
-        .start("https://en.wikipedia.org/wiki/Wikipedia")
-        .await;
+fn main() {
+    println!("Hello, world!");
 }
